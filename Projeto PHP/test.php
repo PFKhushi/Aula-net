23789781fef3f151ba2c9ad3b1cc5351969cc312
PFKhushi <110544443+PFKhushi@users.noreply.github.com>
2022-11-29 23:30:10 -0300

    Base
    feita

---
+++ Projeto PHP/test.php
@@ -0,0 +1,17 @@
+<?php
+
+include_once("topo.php");
+
+
+if(empty($_SERVER["QUERY_STRING"])){
+    $var = "home.php";
+    include_once("$var");
+
+}else {
+    $pg = $_GET['pg'];
+    include_once("$pg.php");
+}
+
+
+include_once("rodape.php");
+?>
