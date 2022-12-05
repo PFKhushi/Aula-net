7f3ec1a09523d25f611d11cd9864d392605f66f6
PFKhushi <110544443+PFKhushi@users.noreply.github.com>
2022-12-05 04:54:47 -0300

    Add files via upload
    Sessao respondendo certinho c:

---
+++ Projeto_php_agr_vai/Projeto_php_agr_vai/index.php
@@ -0,0 +1,19 @@
+<?php
+
+session_start();
+
+
+include_once("topo.php");
+
+if(empty($_SERVER["QUERY_STRING"])){
+    $var = "inicio.php";
+    include_once("$var");
+
+}else {
+    $pg = $_GET['pg'];
+    include_once("$pg.php");
+}
+
+
+include_once("rodape.php");
+?>
