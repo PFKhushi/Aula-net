c9f89a007ff206fdfcd3da67dab82323f3db6737
PFKhushi <110544443+PFKhushi@users.noreply.github.com>
2022-12-06 03:08:12 -0300

    Add files via upload
    tinha apagado um fetch, tudo normal agr

--- Projeto_php_agr_vai/includes/dbeditor.inc.php
+++ Projeto_php_agr_vai/includes/dbeditor.inc.php
@@ -4,42 +4,48 @@ require 'dbhandler.inc.php';
 
 $consulta = "SELECT * FROM aboutus"; 
 $cons = mysqli_query($conn, $consulta);
-
-
+$cons_data = mysqli_fetch_assoc($cons);
 
 
 if(isset($_POST['submit-title'])){
 
-    $change_title = $_POST['title'];
-    $sql = "UPDATE aboutus SET Title='$change_title'";
-    
-    header("Location: ../?pg=AdminSettings");
-
+    $change = $_POST['title'];
+    $sql = "UPDATE aboutus SET Title='$change' WHERE ID=1";
+    if ($conn->query($sql) === TRUE) {
+        header("Location: ../?pg=testgood");
+      } else {
+        header("Location: ../?pg=testnotgood");
+      }
 
 }else if(isset($_POST['submit-content'])){
 
-    $change_content = $_POST['content'];
-    $sql = "UPDATE aboutus SET Content='$change_content'";
-    
-    header("Location: ../?pg=AdminSettings");
-
+    $change = $_POST['content'];
+    $sql = "UPDATE aboutus SET Content='$change' WHERE ID=1";
+    if ($conn->query($sql) === TRUE) {
+        header("Location: ../?pg=testgood");
+      } else {
+        header("Location: ../?pg=testnotgood");
+      }
 
 }else if(isset($_POST['submit-mail'])){
 
-    $change_mail = $_POST['mail'];
-    $sql = "UPDATE aboutus SET CttMail='$change_mail'";
-    
-    header("Location: ../?pg=AdminSettings");
+    $change = $_POST['mail'];
+    $sql = "UPDATE aboutus SET CttMail='$change' WHERE ID=1";
+    if ($conn->query($sql) === TRUE) {
+        header("Location: ../?pg=testgood");
+      } else {
+        header("Location: ../?pg=testnotgood");
+      }
 
 }else if(isset($_POST['submit-number'])){
 
-    $change_number = $_POST['number'];
-    $sql = "UPDATE aboutus SET CttNum='$change_number'";
+    $change = $_POST['number'];
+    $sql = "UPDATE aboutus SET CttNum='$change' WHERE ID=1";
     
-    header("Location: ../?pg=AdminSettings");
-
 
 }else{
     header("Location: ../?pg=ErrorCred");
     exit();
 }
+
+$cons_data = mysqli_fetch_assoc($cons);
