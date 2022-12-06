dea9c2869b530de0db1805752a3b3f02bc13697d
PFKhushi <110544443+PFKhushi@users.noreply.github.com>
2022-12-06 03:15:35 -0300

    Add files via upload
    consigo atualizar titulo, mas resto é ignorado

--- Projeto_php_agr_vai/includes/dbeditor.inc.php
+++ Projeto_php_agr_vai/includes/dbeditor.inc.php
@@ -11,37 +11,29 @@ if(isset($_POST['submit-title'])){
 
     $change = $_POST['title'];
     $sql = "UPDATE aboutus SET Title='$change' WHERE ID=1";
-    if ($conn->query($sql) === TRUE) {
-        header("Location: ../?pg=testgood");
-      } else {
-        header("Location: ../?pg=testnotgood");
-      }
+    $conn->query($sql);
+    header("Location: ../?pg=AdminSettings");
 
 }else if(isset($_POST['submit-content'])){
 
     $change = $_POST['content'];
     $sql = "UPDATE aboutus SET Content='$change' WHERE ID=1";
-    if ($conn->query($sql) === TRUE) {
-        header("Location: ../?pg=testgood");
-      } else {
-        header("Location: ../?pg=testnotgood");
-      }
+    $conn->query($sql);
+    header("Location: ../?pg=AdminSetting");
 
 }else if(isset($_POST['submit-mail'])){
 
     $change = $_POST['mail'];
     $sql = "UPDATE aboutus SET CttMail='$change' WHERE ID=1";
-    if ($conn->query($sql) === TRUE) {
-        header("Location: ../?pg=testgood");
-      } else {
-        header("Location: ../?pg=testnotgood");
-      }
+    $conn->query($sql);
+    header("Location: ../?pg=AdminSettings");
 
 }else if(isset($_POST['submit-number'])){
 
     $change = $_POST['number'];
     $sql = "UPDATE aboutus SET CttNum='$change' WHERE ID=1";
-    
+    $conn->query($sql);
+    header("Location: ../?pg=AdminSettings");
 
 }else{
     header("Location: ../?pg=ErrorCred");
